d95aa0e03eb5697e73e2aebfdce5f7b16a6a68ba
Dheeraj <dheerajkumar1994@gmail.com>
2019-09-20 17:04:07 +0530

    Updated

--- server.py
+++ server.py
@@ -6,25 +6,64 @@ import tornado.websocket
 import wave
 import uuid
 import gc
-from opus.decoder import Decoder as OpusDecoder
+import sys
+import scipy.io.wavfile
+sys.path.append("api")
+import Vokaturi
+from shutil import copyfile
 
+#from opus.decoder import Decoder as OpusDecoder
+Vokaturi.load("api/OpenVokaturi-3-3-win64.dll")
+print ("Analyzed by: %s" % Vokaturi.versionAndLicense())
+
+
+def analyze():
+    file_name = "tempFile.wav"
+    (sample_rate, samples) = scipy.io.wavfile.read(file_name)
+    buffer_length = len(samples)
+    c_buffer = Vokaturi.SampleArrayC(buffer_length)
+    if samples.ndim == 1:  # mono
+            c_buffer[:] = samples[:] / 32768.0
+    else:  # stereo
+            c_buffer[:] = 0.5*(samples[:,0]+0.0+samples[:,1]) / 32768.0
+    voice = Vokaturi.Voice (sample_rate, buffer_length)
+    voice.fill(buffer_length, c_buffer)
+    quality = Vokaturi.Quality()
+    emotionProbabilities = Vokaturi.EmotionProbabilities()
+    voice.extract(quality, emotionProbabilities)
+    
+    if quality.valid:
+            print ("Neutral: %.3f" % emotionProbabilities.neutrality)
+            print ("Happy: %.3f" % emotionProbabilities.happiness)
+            print ("Sad: %.3f" % emotionProbabilities.sadness)
+            print ("Angry: %.3f" % emotionProbabilities.anger)
+            print ("Fear: %.3f" % emotionProbabilities.fear)
+    else:
+            print ("Not enough sonorancy to determine emotions")
+    return (emotionProbabilities.neutrality,emotionProbabilities.happiness,emotionProbabilities.sadness,emotionProbabilities.anger,emotionProbabilities.fear)
+
+
+clients = {}
 class OpusDecoderWS(tornado.websocket.WebSocketHandler):
     
     def open(self):
-
         print('new connection')
+        self.client_id = str(uuid.uuid4())
         self.initialized = False
+        clients[self.client_id] = self
+        print(clients)
+        self.count = 0
+        self.runtimeWriter = False
+        #self.write_message("The server says: 'Hello'. Connection was accepted.")
 
     def my_init(self, msg) :
 
-        print(msg)
+#        print(msg)
         rate, is_encoded, op_rate, op_frm_dur = [int(i) for i in msg.split(',')]
         #rate : actual sampling rate
         #op_rate : the rate we told opus encoder
         #op_frm_dur : opus frame duration
-
         filename = str(uuid.uuid4()) + '.wav'
-
         wave_write = wave.open(filename, 'wb')
         wave_write.setnchannels(1)
         wave_write.setsampwidth(2) #int16, even when not encoded
@@ -34,7 +73,8 @@ class OpusDecoderWS(tornado.websocket.WebSocketHandler):
             self.wave_write.close()
 
         self.is_encoded = is_encoded
-        self.decoder = OpusDecoder(op_rate, 1)
+#        self.decoder = OpusDecoder(op_rate, 1)
+        self.rate = rate
         self.frame_size = op_frm_dur * op_rate
         self.wave_write = wave_write
         self.initialized = True
@@ -53,26 +93,52 @@ class OpusDecoderWS(tornado.websocket.WebSocketHandler):
                 gc.collect()
 
             else :
+#                print(len(data))
+                self.analyzeStream(data)
                 self.wave_write.writeframes(data)
 
     def on_close(self):
 
         if self.initialized :
             self.wave_write.close()
-
+        print(clients)
         print('connection closed')
 
+    def analyzeStream(self,data):
+        if self.count < 120:
+            if self.runtimeWriter is False:
+                filename = 'tempFile.wav'
+                self.wave_write1 = wave.open(filename, 'wb')
+                self.runtimeWriter = True
+                self.wave_write1.setnchannels(1)
+                self.wave_write1.setsampwidth(2) #int16, even when not encoded
+                self.wave_write1.setframerate(self.rate)
+                print("in+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            self.wave_write1.writeframes(data)
+            self.count = self.count + 1
+        elif self.count == 120:
+            if self.runtimeWriter is True:
+                print("out+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                neutrality,happiness,sadness,anger,fear = analyze()
+                self.write_message({'neutral':float(neutrality),'happy':float(happiness),'sad':float(sadness),'anger':float(anger),'fear':float(fear)})
+                self.wave_write1.close()
+                #copyfile('tempFile.wav', str(uuid.uuid4()) +'-'+ str(neutrality) + '-'+ str(happiness)+'-' + str(sadness) + '-' + str(anger) + '-'+ str(fear))
+            self.count = 0
+            self.runtimeWriter = False
+        else:
+            self.count = 0
+        
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("www/index.html")
 
-app = tornado.web.Application([
+app = tornado. web.Application([
     (r'/ws', OpusDecoderWS),
     (r'/', MainHandler),
     (r'/(.*)', tornado.web.StaticFileHandler, { 'path' : './www' })
 ])
 
 http_server = tornado.httpserver.HTTPServer(app)
-http_server.listen(int(os.environ.get('PORT', 8888)))
+http_server.listen(int(os.environ.get('PORT', 8881)))
 print('http server started')
 tornado.ioloop.IOLoop.instance().start()
--- www/main.js
+++ www/main.js
@@ -1,7 +1,132 @@
 var isRecording = false, encode = false;
-var wsh = new WebSocket( 'ws://' + window.location.href.split( '/' )[2] + '/ws' );
+var wsh = new WebSocket( 'ws://localhost:8881/ws' );
 
-function onWsMessage( msg ){ console.log(msg); }
+//Intialiazation 
+var radialObj = radialIndicator('#indicatorContainer', {
+     barColor: {
+        0: '#FF0000',
+        33: '#FFFF00',
+        66: '#0066FF',
+        100: '#33CC33'
+    },
+    radius: (screen.height*12)/100,
+    barWidth : 10,
+    initValue : 40
+});
+
+var contextGraph = document.getElementById('myLineChart');
+// used for example purposes
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// this post id drives the example data
+var postId = 1;
+
+var myChart = new Chart(contextGraph, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      data: [],
+      borderWidth: 5,
+      borderColor:'#00c0ef',
+    }]
+  },
+  options: {
+    grid: {show: false},
+    responsive: true,
+    title: {
+      //display: true,
+      //text: "Chart.js - Dynamically Update Chart Via Ajax Requests",
+    },
+    legend: {
+      display: false
+    },
+    scales: {
+        xAxes: [{
+            display: true,
+            showGrid: false,
+            scaleLabel: {
+                display: true,
+                labelString: 'Voice Samples'
+            },
+        splitLine: {
+                show: false
+            },
+        gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+            }
+        }],
+        yAxes: [{
+            display: false,
+            scaleLabel: {
+                display: false,
+                labelString: 'Confidence'
+            },
+        splitLine: {
+                show: false
+            },
+        gridLines: {
+                color: "rgba(0, 0, 0, 0)",
+            }
+        }]
+    }
+  }
+});
+
+// // logic to get new data
+// var getData = function() {
+//   $.ajax({
+//     url: 'https://jsonplaceholder.typicode.com/posts/' + postId + '/comments',
+//     success: function(data) {
+//       // process your data to pull out what you plan to use to update the chart
+//       // e.g. new label and a new data point
+      
+//       // add new label and data point to chart's underlying data structures
+//       myChart.data.labels.push("Post " + postId++);
+//       myChart.data.datasets[0].data.push(getRandomIntInclusive(1, 25));
+      
+//       // re-render the chart
+//       myChart.update();
+//     }
+//   });
+// };
+
+// // get new data every 3 seconds
+// setInterval(getData, 3000);
+
+
+// Random data
+//var line1 = new TimeSeries();
+//var line2 = new TimeSeries();
+// setInterval(function() {
+//   line1.append(new Date().getTime(), Math.random());
+//   line2.append(new Date().getTime(), Math.random());
+// }, 1000);
+
+function onWsMessage( msg ){ 
+    //line1.append(new Date().getTime(), msg.data.sad);
+    //line2.append(new Date().getTime(), msg.data.neutral);
+    
+    var obj = JSON.parse(msg.data);
+    //line1.append(new Date().getTime(), obj.anger);
+    //Using Instance
+    radialObj.animate(obj.neutral*100); 
+    myChart.data.labels.push(postId++);
+    myChart.data.datasets[0].data.push(obj.neutral*100);
+    myChart.update();
+    console.log(obj);
+}
+
+
+// var smoothie = new SmoothieChart({millisPerPixel:58,maxValueScale:1.5,minValueScale:1.5,grid:{fillStyle:'#ffffff',strokeStyle:'#ffffff',sharpLines:true}});
+// smoothie.addTimeSeries(line1, { strokeStyle: 'rgb(0, 255, 0)', lineWidth: 1 });
+// //smoothie.addTimeSeries(line2, { strokeStyle: 'rgb(255, 0, 255)', fillStyle: 'rgba(255, 0, 255, 0.3)', lineWidth: 3 });
+
+// smoothie.streamTo(document.getElementById("mycanvas"), 1000);
 
 wsh.onmessage = onWsMessage;
 var ap = new OpusEncoderProcessor( wsh );
@@ -9,10 +134,10 @@ var mh = new MediaHandler( ap );
 
 function sendSettings()
 {
-    if( document.getElementById( "encode" ).checked )
-	encode = 1;
+    if( false )
+    encode = 1;
     else
-	encode = 0;
+    encode = 0;
 
     var rate = String( mh.context.sampleRate / ap.downSample );
     var opusRate = String( ap.opusRate );
@@ -26,6 +151,7 @@ function sendSettings()
 function startRecord()
 {
     document.getElementById( "record").innerHTML = "Stop";
+    document.getElementById( "record").className = "Rec";
     document.getElementById( "encode" ).disabled = true;
     mh.context.resume(); // needs an await?
     sendSettings();
@@ -37,6 +163,7 @@ function stopRecord()
 {
     isRecording  = false;
     document.getElementById( "record").innerHTML = "Record";
+    document.getElementById( "record").className = "notRec";
     document.getElementById( "encode" ).disabled = false;
     console.log( 'ended recording' );    
 }
@@ -44,7 +171,7 @@ function stopRecord()
 function toggleRecord()
 {
     if( isRecording )
-	stopRecord();
+    stopRecord();
     else
-	startRecord();
+    startRecord();
 }
